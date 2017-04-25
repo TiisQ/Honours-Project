6651ccf0f62769ee3e698da3e81c258bc35059f3
TiiSQ <tdaumas@ska.ac.za>
2017-04-25 16:15:50 +0200

    Add files via upload

---
+++ ProjectPacks.py
@@ -0,0 +1,61 @@
+import numpy as np
+def getSParams(ArrayFile,FieldFile): ## must be strings
+	array= {}
+	field= {}
+	with open(ArrayFile,'r') as ainfo:
+		for line in ainfo:
+			key, value= line.split(':')
+			array[key]= value.strip()
+        
+	with open(FieldFile,'r') as finfo:
+		for line in finfo:
+			key, value= line.split(':')
+			field[key]= value.strip()
+	#I might have to further format this to allow for multiple sets of array/field profile information.		
+	return array,field
+
+def formatSParams(ArrayFile,FieldFile,td): ## isn't there a way of declaring this function without arguments?
+	array,field= getSParams(ArrayFile,FieldFile)
+
+	freq = np.float64(array['Centre_Freq'])
+	ArNum = np.uint64(array['Num_Array'])
+	## I may need a conditional/exception statement here incase the Num_Array does not correspond with the Centre_Dist info in file
+
+	arrayHxpos= [np.float64(x) for x in array['Centre_Dist'].split(',')]
+	arrayHxpos.sort()  ## sorting list in ascending order
+
+	[Hmin,Hmax]= [np.float64(h) for h in field['HourAngleInterval'].split(',')]
+	declangle= np.float64(field['FieldCentreDecl'])
+	SNum= np.uint64(field['NumSources'])
+	## I need a conditional statement here incase the NumSources does not correspond with the SourceParams info in file
+	#using floats for the sake of generality. 
+	
+	point_sources=[]
+	Amp=[]
+	l=[]
+	m=[]
+	for sp in field['SourceParams'].split(' '):
+		point_sources.append([np.float64(x) for x in sp.split(',')])
+		
+	for i in range(SNum):
+		Amp.append(point_sources[i][0])
+		l.append(np.radians(point_sources[i][1]))
+		m.append(np.radians(point_sources[i][2]))
+		
+	## baseline as a func of wavelength
+	## I need to save both b12 and b21 for completion of ellipse
+	lamda= 3e8/freq
+	bline= [[(arrayHxpos[i]-arrayHxpos[k])/lamda for i in range(ArNum)]for k in range(ArNum)]
+
+	#setting up my (point source) field  
+	Hangles = np.linspace(Hmin,Hmax,td) # 12 hours because we are saving info about b12 AND b21
+	Ht= 15*np.radians(Hangles)  #Hour angle H(t) has to be in radians which is *pi/12h and since 360deg=24hrs
+	declrad= np.radians(declangle)
+
+	## determining uv co-ordinates for uv tracks
+	ut= [[bline[k][i]*np.cos(Ht) for i in range(ArNum)]for k in range(ArNum)]
+	vt= [[bline[k][i]*np.sin(Ht)*np.sin(declrad) for i in range(ArNum)]for k in range(ArNum)]
+		
+	return Amp,l,m,ut,vt,SNum,ArNum,arrayHxpos
+  
+            
